45d939b15bec2d6e60aba7a53114aefaa71517a1
DmitrijsBalutins <56105943+DmitrijsBalutins@users.noreply.github.com>
2020-02-04 10:15:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -12,11 +13,32 @@ def getHome():
 
 @app.route('/about')
 def about():
-  return render_template('about.html', active_page = 'about')
+  return render_template('about.html')
 
 @app.route('/contact')
 def contact():
   return render_template('contact.html', phone = "123")
+
+
+@app.route('/params')
+def params():
+  return request.args
+
+@app.route('/post_req', methods = ['POST'])
+def post_req():
+  return request.args
+
+@app.route('/read_file')
+def read_from_file():
+  file_content = read_file()
+  return file_content
+
+@app.route('/write_to_file', methods = ['POST'])
+def write_to_file():
+  line = request.form.get('data')
+  write_file(line)
+  return f"Add line '{line}' to file."
+
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", threaded=True, port=5050, debug=True) 
